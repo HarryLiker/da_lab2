d6163b2565bae5101c792c509212acfe8565403a
root <root@Harry.localdomain>
2020-11-25 21:40:05 +0300

    Пока не получается устранить ошибку

---
+++ solution/functions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+int abs(int x);
+
+bool strings_compare(const char *string1, const char *string2);
+
+int equal_strings(const char *string1, const char *string2);
+
+void str_copy(const char *line, char *string);
